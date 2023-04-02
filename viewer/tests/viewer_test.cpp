b85b429e90461260fd6fbdcbe66e18f8992f0310
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2023-04-02 00:58:00 -0700

    Use clang tidy suggestion to convert meaningless member function to static function

--- viewer/tests/viewer_test.cpp
+++ viewer/tests/viewer_test.cpp
@@ -7,14 +7,11 @@ using namespace mv;
 using namespace mv::common;
 using namespace mv::events;
 
-namespace mv {
-    namespace viewer {
-        class ViewerTest {
-        public:
-            void simulateKeyPress(int key) { EventHandler::handleKeyPress(nullptr, key, 0, GLFW_PRESS, 0); }
-        };
-
-    }
+namespace mv::viewer {
+    class ViewerTest {
+    public:
+        static void simulateKeyPress(int key) { EventHandler::handleKeyPress(nullptr, key, 0, GLFW_PRESS, 0); }
+    };
 }
 
 TEST(Viewer, Singleton) {
@@ -26,11 +23,10 @@ TEST(Viewer, Singleton) {
 TEST(Viewer, Getters) {
     Viewer& v = Viewer::getInstance();
     ASSERT_EQ(v.isDisplayingNormals(), false) << "Normals display is supposed to be off by default";
-    mv::viewer::ViewerTest vt;
-    vt.simulateKeyPress(GLFW_KEY_N);
+    mv::viewer::ViewerTest::simulateKeyPress(GLFW_KEY_N);
     ASSERT_EQ(v.isDisplayingNormals(), true) << "Normals display is not true";
-    vt.simulateKeyPress(GLFW_KEY_W);
+    mv::viewer::ViewerTest::simulateKeyPress(GLFW_KEY_W);
     ASSERT_EQ(v.getRenderMode(), mv::Viewer::RenderMode::Wireframe) << "Render mode is not wireframe";
-    vt.simulateKeyPress(GLFW_KEY_S);
+    mv::viewer::ViewerTest::simulateKeyPress(GLFW_KEY_S);
     ASSERT_EQ(v.getRenderMode(), mv::Viewer::RenderMode::Shaded) << "Render mode is not shaded";
 }
