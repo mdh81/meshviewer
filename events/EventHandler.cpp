4da1cdb31e2d3efb808d0e9215adb3ef8970505a
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2023-05-12 15:27:51 -0700

    Handle mouse events

--- events/EventHandler.cpp
+++ events/EventHandler.cpp
@@ -11,13 +11,24 @@ namespace mv { namespace events {
 bool EventHandler::m_started = false;
 EventHandler::EventCallbackMap EventHandler::m_eventCallbackMap;
 
-void EventHandler::handleKeyPress(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (action != GLFW_PRESS) return;
-    auto lookupRes = m_eventCallbackMap.find(Event(key, mods));
-    if (lookupRes != m_eventCallbackMap.end()) {
-        lookupRes->second.get().call();
+void EventHandler::handleKeyPress(GLFWwindow* window, int key, int scancode, int action, int modifiers) {
+    if (action == GLFW_PRESS) {
+        handleKeyOrMouseEvent(key, modifiers);
+    }
+}
+
+void EventHandler::handleMouseEvent(GLFWwindow* window, int button, int action, int modifiers) {
+    if (action == GLFW_PRESS) {
+        handleKeyOrMouseEvent(button, modifiers);
+    }
+}
+
+void EventHandler::handleKeyOrMouseEvent(int keyOrButtonIdentifier, int modifiers) {
+    auto callback = getEventHandler(keyOrButtonIdentifier, modifiers);
+    if (callback) {
+        callback->call();
     } else {
-       cerr << "No callback associated with " << key << " and modifier " << mods << endl;
+        cerr << "No callback associated with " << keyOrButtonIdentifier << " and modifier " << modifiers << endl;
     }
 }
 
@@ -28,10 +39,19 @@ void EventHandler::registerCallback(const Event& event, const Callback& callback
 void EventHandler::start(GLFWwindow* window) {
     if (!m_started) {
         glfwSetKeyCallback(window, &handleKeyPress);
+        glfwSetMouseButtonCallback(window, &handleMouseEvent);
         m_started = true;
     } else {
         runtime_error("Event handler already initialized");
     }
 }
 
+Callback const* EventHandler::getEventHandler(int const glfwKeyOrButton, int const mods) {
+    Callback const* callback = nullptr;
+    auto lookupRes = m_eventCallbackMap.find(Event(glfwKeyOrButton, mods));
+    if (lookupRes != m_eventCallbackMap.end()) {
+        callback = &(lookupRes->second.get());
+    }
+    return callback;
+}
 } }
--- events/tests/event_handler_test.cpp
+++ events/tests/event_handler_test.cpp
@@ -15,6 +15,7 @@ namespace mv { namespace events {
 class EventHandlerTest {
     public:
         void simulateKeyPress(int key) { EventHandler::handleKeyPress(nullptr, key, 0, GLFW_PRESS, 0); }
+        void simulateMouseButtonPress(int button, int modifiers) { EventHandler::handleMouseEvent(nullptr, button, GLFW_PRESS, modifiers); }
 };
 
 } }
@@ -23,6 +24,8 @@ class Listener {
     public:
         void handleOKey() { cout << "O was pressed"; }
         void handlePKey() { cout << "P was pressed"; }
+        void handleLeftMouse() { cout << "Left mouse was pressed"; }
+        void handleMiddleMouseWithShiftDown() { cout << "Middle mouse was pressed with shift down"; }
 };
 
 
@@ -33,10 +36,20 @@ TEST(EventHandler, Callback) {
                         CallbackFactory::getInstance().registerCallback(t, &Listener::handleOKey)); 
     eh.registerCallback(Event{GLFW_KEY_P},
                         CallbackFactory::getInstance().registerCallback(t, &Listener::handlePKey));
+    eh.registerCallback(Event {GLFW_MOUSE_BUTTON_LEFT},
+                        CallbackFactory::getInstance().registerCallback(t, &Listener::handleLeftMouse));
+    eh.registerCallback(Event {GLFW_MOUSE_BUTTON_MIDDLE, GLFW_MOD_SHIFT},
+                        CallbackFactory::getInstance().registerCallback(t, &Listener::handleMiddleMouseWithShiftDown));
     testing::internal::CaptureStdout();
     EventHandlerTest().simulateKeyPress(GLFW_KEY_O);
     ASSERT_EQ(testing::internal::GetCapturedStdout(), "O was pressed");
     testing::internal::CaptureStdout();
     EventHandlerTest().simulateKeyPress(GLFW_KEY_P);
     ASSERT_EQ(testing::internal::GetCapturedStdout(), "P was pressed");
+    testing::internal::CaptureStdout();
+    EventHandlerTest().simulateMouseButtonPress(GLFW_MOUSE_BUTTON_LEFT, 0);
+    ASSERT_EQ(testing::internal::GetCapturedStdout(), "Left mouse was pressed");
+    testing::internal::CaptureStdout();
+    EventHandlerTest().simulateMouseButtonPress(GLFW_MOUSE_BUTTON_MIDDLE, GLFW_MOD_SHIFT);
+    ASSERT_EQ(testing::internal::GetCapturedStdout(), "Middle mouse was pressed with shift down");
 }
