d3b3c824be3861b61b102d771af4addd6c80a389
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2023-11-30 21:28:57 -0800

    Remove unused headers

--- objects/Camera.h
+++ objects/Camera.h
@@ -1,9 +1,7 @@
 #pragma once
 
 #include <memory>
-#include "GL/glew.h"
 #include "Renderable.h"
-#include "Types.h"
 #include "glm/matrix.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include <iostream>
