77513483ad1e972bbd75174907767503a8843d3d
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-01-23 09:22:19 -0800

    Fix typo

--- common/Types.h
+++ common/Types.h
@@ -24,51 +24,49 @@
 // Definitions of types that are common among various pieces of the meshviewer application
 namespace mv::common {
 
-using Point3D = math3d::Vector3<float>;
-using Vector3D = math3d::Vector3<float>;
-using Point2D = math3d::Vector2<float>;
-using Vector2D = math3d::Vector2<float>;
-using Color = Point3D;
-using Line = std::vector<Point3D>;
-using Lines = std::vector<Line>;
-using Points = std::vector<Point3D>;
-using Bounds = math3d::Bounds3D<float>;
-using OrthographicProjectionMatrix = math3d::OrthographicProjectionMatrix<float>;
-using TransformMatrix = math3d::Matrix<float, 4, 4>;
-using Vector4D = math3d::Vector4<float>;
+    using Point3D = math3d::Vector3<float>;
+    using Vector3D = math3d::Vector3<float>;
+    using Point2D = math3d::Vector2<float>;
+    using Vector2D = math3d::Vector2<float>;
+    using Color = Point3D;
+    using Line = std::vector<Point3D>;
+    using Lines = std::vector<Line>;
+    using Points = std::vector<Point3D>;
+    using Bounds = math3d::Bounds3D<float>;
+    using OrthographicProjectionMatrix = math3d::OrthographicProjectionMatrix<float>;
+    using TransformMatrix = math3d::Matrix<float, 4, 4>;
+    using Vector4D = math3d::Vector4<float>;
 
-inline std::ostream& operator<<(std::ostream& os, const Point3D& v) {
-    os << "[" << v.x << "," << v.y << "," << v.z << "]";
-    return os;
-}
+    inline std::ostream &operator<<(std::ostream &os, const Point3D &v) {
+        os << "[" << v.x << "," << v.y << "," << v.z << "]";
+        return os;
+    }
 
-template<typename T>
-inline std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
-    for (auto& item : vec)
-        os << item << ' ';
-    return os;
-}
+    template<typename T>
+    inline std::ostream &operator<<(std::ostream &os, const std::vector<T> &vec) {
+        for (auto &item: vec)
+            os << item << ' ';
+        return os;
+    }
 
 // A 2D array whose data can be shared among instances of this type. The data
 // is stored as a 1D array to facilitate easy transfer to OpenGL APIs.
-template<typename T, unsigned tupleSize=1>
-class Array {
+    template<typename T, unsigned tupleSize = 1>
+    class Array {
     private:
         size_t m_size;
         size_t m_offset;
         std::shared_ptr<T[]> m_data;
     public:
         explicit Array(size_t numElements)
-            : m_size(numElements)
-            , m_offset(0)
-            , m_data(new T[m_size * tupleSize], std::default_delete<T[]>()) {
+                : m_size(numElements), m_offset(0), m_data(new T[m_size * tupleSize], std::default_delete<T[]>()) {
         }
 
         [[nodiscard]] size_t getSize() const { return m_size; }
 
         [[nodiscard]] size_t getDataSize() const { return m_size * tupleSize * sizeof(T); }
 
-        T const* getData() const { return m_data.get(); }
+        T const *getData() const { return m_data.get(); }
 
         // TODO: Assert that all the parameters are of the same type
         template<typename... Types>
@@ -85,7 +83,7 @@ class Array {
             }
         }
 
-        T& operator[](unsigned const index) {
+        T &operator[](unsigned const index) {
             if (index < m_size * tupleSize) {
                 return m_data[index];
             } else {
@@ -94,7 +92,7 @@ class Array {
         }
 
     private:
-        void append(std::initializer_list<T> const& list) {
+        void append(std::initializer_list<T> const &list) {
             if (list.size() != tupleSize) {
                 throw std::invalid_argument("Tuple size is " +
                                             std::to_string(tupleSize) +
@@ -105,113 +103,120 @@ class Array {
             m_offset += tupleSize;
         }
 
-};
+    };
 
-using Triangles = std::vector<Array<unsigned, 3>>;
+    using Triangles = std::vector<Array<unsigned, 3>>;
 
-using VertexIndices = std::vector<unsigned>;
-using VertexIndex = unsigned;
+    using VertexIndices = std::vector<unsigned>;
+    using VertexIndex = unsigned;
 
-template <typename T, T beginVal, T endVal>
-class EnumIterator {
-    // Underlying type's alias
-    using ULType = typename std::underlying_type<T>::type;
-    // An integer to maintain the iterator's position
-    ULType m_val;
+    template<typename T, T beginVal, T endVal>
+    class EnumIterator {
+        // Underlying type's alias
+        using ULType = typename std::underlying_type<T>::type;
+        // An integer to maintain the iterator's position
+        ULType m_val;
     public:
         // Conversion constructor to convert enumerator to iterator type
-        explicit EnumIterator(const T& f) : m_val(static_cast<ULType>(f)) {}
+        explicit EnumIterator(const T &f) : m_val(static_cast<ULType>(f)) {}
+
         // Default constructor that initilizes the iterator to point to the first enumerator
         EnumIterator() : m_val(static_cast<ULType>(beginVal)) {}
+
         // Postfix increment operator
         EnumIterator operator++() {
             ++m_val;
             return *this;
         }
+
         // Dereference operator
         T operator*() { return static_cast<T>(m_val); }
+
         // Begin and end methods
         EnumIterator begin() { return *this; }
-        EnumIterator end() { return ++EnumIterator(endVal); }
-        bool operator!=(const EnumIterator& other) { return m_val != other.m_val; }
-};
 
-using byte = char;
+        EnumIterator end() { return ++EnumIterator(endVal); }
+
+        bool operator!=(const EnumIterator &other) { return m_val != other.m_val; }
+    };
+
+    using byte = char;
 
 // TODO: It makes more sense to move these definitions to specific types (e.g. Glyph)
-enum class Axis {
-    X,
-    Y,
-    Z,
-    Arbitrary
-};
+    enum class Axis {
+        X,
+        Y,
+        Z,
+        Arbitrary
+    };
 
-struct WindowDimensions {
-    unsigned width;
-    unsigned height;
-};
+    struct WindowDimensions {
+        unsigned width;
+        unsigned height;
+    };
 
 #ifdef EMSCRIPTEN
-using CanvasDimensions = WindowDimensions;
+    using CanvasDimensions = WindowDimensions;
 #endif
 
-enum class NormalLocation {
-    Face,
-    Vertex
-};
-
-struct GlyphDecorator {
-    enum class Style {
-        Line,
-        Arrow
+    enum class NormalLocation {
+        Face,
+        Vertex
     };
-    Style style;
-    Color color;
-    float length;
-    std::optional<float> arrowRadius;
-    std::optional<float> arrowLength;
-};
 
-enum class GlyphAssociation {
-    Undefined,
-    FaceNormal,
-    VertexNormal
-};
+    struct GlyphDecorator {
+        enum class Style {
+            Line,
+            Arrow
+        };
+        Style style;
+        Color color;
+        float length;
+        std::optional<float> arrowRadius;
+        std::optional<float> arrowLength;
+    };
 
-unsigned const MOUSE_WHEEL_EVENT = 1000;
+    enum class GlyphAssociation {
+        Undefined,
+        FaceNormal,
+        VertexNormal
+    };
 
-enum class Direction {
-    Left,
-    Right,
-    Up,
-    Down,
-    Forward,
-    Backward
-};
+    unsigned const MOUSE_WHEEL_EVENT = 1000;
 
-inline
-std::ostream& operator<<(std::ostream& os, Direction const direction) {
-    switch (direction) {
-        case Direction::Left:
-            os << "Left";
-            break;
-        case Direction::Right:
-            os << "Right";
-            break;
-        case Direction::Up:
-            os << "Up";
-            break;
-        case Direction::Down:
-            os << "Down";
-            break;
-        case Direction::Forward:
-            os << "Forward";
-            break;
-        case Direction::Backward:
-            os << "Backward";
-            break;
-        default:
-            os << "Unknown";
+    enum class Direction {
+        Left,
+        Right,
+        Up,
+        Down,
+        Forward,
+        Backward
+    };
+
+    inline
+    std::ostream &operator<<(std::ostream &os, Direction const direction) {
+        switch (direction) {
+            case Direction::Left:
+                os << "Left";
+                break;
+            case Direction::Right:
+                os << "Right";
+                break;
+            case Direction::Up:
+                os << "Up";
+                break;
+            case Direction::Down:
+                os << "Down";
+                break;
+            case Direction::Forward:
+                os << "Forward";
+                break;
+            case Direction::Backward:
+                os << "Backward";
+                break;
+            default:
+                os << "Unknown";
+        }
+        return os;
     }
-    return os;
 }
