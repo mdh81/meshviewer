25be963538cf460480cc484bcefa5f44afee9574
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-01-29 22:00:37 -0800

    Refine pointer types to make sure type conversions to weak and raw pointers are handled correctly. Also, use the using trick to inherit constructors.

--- common/PointerTypes.h
+++ common/PointerTypes.h
@@ -19,15 +19,39 @@ namespace mv::common {
     // A shared pointer wrapper
     template<typename T>
     struct SharedPointer : SmartPointer, std::shared_ptr<T> {
+       // Inherit constructors
+       using std::shared_ptr<T>::shared_ptr;
+
         template<typename... ArgumentTypes>
         SharedPointer(ArgumentTypes&&... arguments) : std::shared_ptr<T>(std::forward<ArgumentTypes>(arguments)...) {}
+
+       // Conversion constructor to build a shared pointer of base type from a raw pointer to a derived instance
+       template<typename DerivedType>
+       SharedPointer(DerivedType* derivedPtr) : std::shared_ptr<T>(static_cast<T*>(derivedPtr)) {}
+
+       // Conversion constructor to build a shared pointer from a weak pointer of the same type
+       SharedPointer(std::weak_ptr<T> const& weakPtr) : std::shared_ptr<T>{weakPtr.lock()} {
+       }
+
+       // Down cast to a weak pointer of derived type
+       template<typename Derived>
+       std::weak_ptr<Derived> asWeakPointer() const {
+           static_assert(std::is_base_of_v<T, Derived>, "Can only downcast to inherited types");
+           auto derivedPointer = std::dynamic_pointer_cast<Derived>(*this);
+           return std::weak_ptr<Derived>(derivedPointer);
+       }
+
     };
 
     // A unique pointer wrapper
     template<typename T>
     struct UniquePointer : SmartPointer, std::unique_ptr<T> {
+        // Inherit constructors
+        using std::unique_ptr<T>::unique_ptr;
+
         template<typename... ArgumentTypes>
         UniquePointer(ArgumentTypes&&... arguments) : std::unique_ptr<T>(std::forward<ArgumentTypes>(arguments)...) {}
+
     };
 
     // Hash value for a smart pointer is the address of the pointee
