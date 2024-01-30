881bbeea9ab36dc2624c6dc9f95a95a8cefa9793
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-01-29 21:46:30 -0800

    Refactored callback factory to ensure only MeshViewerObject instances and their member functions can be registered as callback targets. The callback factory also now triggers the callback observers when callbacks are created.

--- events/CallbackFactory.h
+++ events/CallbackFactory.h
@@ -2,6 +2,7 @@
 
 #include "Callbacks.h"
 #include "MeshViewerObject.h"
+#include "ObjectRegistry.h"
 #include "PointerTypes.h"
 #include <unordered_set>
 
@@ -10,30 +11,62 @@ namespace mv::events {
 class CallbackFactory {
 
 public:
+    // NOTE: While these two methods appear like good candidates to be consolidated into a single createCallback() method,
+    //       it's critical to note that FunctionPointerType is not compatible between BasicEventCallback and DataEventCallback.
+    //       Therefore, we cannot instantiate these two class templates in a function template that has function pointer as
+    //       a parameter
     template<typename InstanceType, typename FunctionPointerType, typename... ArgumentTypes>
-    BasicEventCallback::Pointer const& createCallback(InstanceType& instance, FunctionPointerType functionPointer, ArgumentTypes&&... arguments) {
-        if (sizeof...(ArgumentTypes)) {
-            return createBasicEventCallback(instance, functionPointer, std::forward<ArgumentTypes>(arguments)...);
+    BasicEventCallback::ObservingPointer createBasicEventCallback(InstanceType& instance, FunctionPointerType functionPointer, ArgumentTypes&&... arguments) {
+        static_assert(std::is_base_of_v<MeshViewerObject, InstanceType>,
+                "Callbacks can only be created for instances of MeshViewerObject. "
+                "This is to ensure automatic cleanup and to eliminate chances of dangling references");
+        auto [itr, status] =
+                callbacks.emplace(new BasicEventCallbackImpl(instance, functionPointer, std::forward<ArgumentTypes>(arguments)...));
+        notifyCreationObservers(instance, *itr);
+        return itr->template asWeakPointer<BasicEventCallback>();
+    }
+
+    template<typename InstanceType, typename FunctionPointerType>
+    DataEventCallback::ObservingPointer createDataEventCallback(InstanceType& instance, FunctionPointerType functionPointer) {
+        static_assert(std::is_base_of_v<MeshViewerObject, InstanceType>,
+                      "Callbacks can only be created for instances of MeshViewerObject. "
+                      "This is to ensure automatic cleanup and to eliminate chances of dangling references");
+        auto [itr, status] =
+                callbacks.emplace(new DataEventCallbackImpl(instance, functionPointer));
+        notifyCreationObservers(instance, *itr);
+        return itr->template asWeakPointer<DataEventCallback>();
+    }
+
+    bool hasCallbackCreationObserver() const {
+        return !callbackCreationObservers.empty();
+    }
+
+    void addCallbackCreationObserver(CallbackCreationObserver& callbackCreationObserver) {
+        callbackCreationObservers.emplace_back(std::ref(callbackCreationObserver));
+    }
+
+    void removeCallback(Callback::ObservingPointer callback) {
+        auto itr = callbacks.find(callback);
+        if (itr != callbacks.end()) {
+            callbacks.erase(itr);
+        } else {
+            throw std::runtime_error("Cannot remove callback: The callback requested for removal is not a known Callback");
         }
     }
 
-    void removeCallback(BasicEventCallback::Pointer const& basicEventCallback) {
-        basicEventCallbacks.erase(basicEventCallbacks.find(basicEventCallback));
+private:
+    void notifyCreationObservers(MeshViewerObject const& instance, common::SharedPointer<Callback> callback) const {
+        for (auto& callbackCreationObserver : callbackCreationObservers) {
+            callbackCreationObserver.get().notifyCallbackCreated(instance, callback);
+        }
     }
 
 private:
-    template<typename InstanceType, typename FunctionPointerType, typename... ArgumentTypes>
-    BasicEventCallback::Pointer const& createBasicEventCallback(InstanceType& instance, FunctionPointerType functionPointer, ArgumentTypes&&... arguments) {
-        auto [itr, status] = basicEventCallbacks.emplace(new BasicEventCallbackImpl(instance, functionPointer, std::forward<ArgumentTypes>(arguments)...));
-        return *itr;
-    }
+    using Callbacks = std::unordered_set<Callback::Pointer, common::SmartPointerHasher, common::SmartPointerComparator>;
+    static inline Callbacks callbacks{};
 
-private:
-    static inline
-    std::unordered_set<
-            BasicEventCallback::Pointer,
-            common::SmartPointerHasher,
-            common::SmartPointerComparator> basicEventCallbacks{};
+    using CallbackCreationObservers = std::vector<std::reference_wrapper<CallbackCreationObserver>>;
+    static inline CallbackCreationObservers callbackCreationObservers;
 
     friend class CallbackFactoryTester;
 };
