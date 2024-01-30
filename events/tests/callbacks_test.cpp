dc6ebeafc41b21451468187bc6324c8de6ca2fb8
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-01-29 21:50:41 -0800

    Test for callback factory. To be renamed once the refactoring is complete.

--- events/tests/callbacks_test.cpp
+++ events/tests/callbacks_test.cpp
@@ -1,39 +1,53 @@
 #include "gtest/gtest.h"
-#include "Callbacks.h"
+#include "Callback.h"
 #include "CallbackFactory.h"
 
 namespace mv::events {
     class CallbackFactoryTester {
     public:
-        auto getNumBasicEventCallbacks() {
-            return mv::events::CallbackFactory{}.basicEventCallbacks.size();
+        auto getNumCallbacks() {
+            return mv::events::CallbackFactory{}.callbacks.size();
         }
     };
 }
 
-struct TestClass {
+struct TestClass : public mv::MeshViewerObject {
     void basicEventCallback(int a, float b, unsigned c) {
-
     }
 
     void dataEventCallback(std::vector<std::any>&& eventData) {
-
     }
 
 };
 
 TEST(CallbackFactory, BasicEventCallbackCreation) {
     TestClass testObject;
-    mv::events::CallbackFactory{}.createCallback(testObject, &TestClass::basicEventCallback, 10, 12.5, 100);
+    mv::events::CallbackFactory{}.createBasicEventCallback(testObject, &TestClass::basicEventCallback, 10, 12.5, 100);
     mv::events::CallbackFactoryTester tester;
-    ASSERT_EQ(tester.getNumBasicEventCallbacks(), 1);
+    ASSERT_EQ(tester.getNumCallbacks(), 1);
+}
+
+TEST(CallbackFactory, DataEventCallbackCreation) {
+    TestClass testObject;
+    mv::events::CallbackFactory{}.createDataEventCallback(testObject, &TestClass::dataEventCallback);
+    mv::events::CallbackFactoryTester tester;
+    ASSERT_EQ(tester.getNumCallbacks(), 1);
 }
 
 TEST(CallbackFactory, BasicEventCallbackRemoval) {
     TestClass testObject;
-    auto& callbackPointer = mv::events::CallbackFactory{}.createCallback(testObject, &TestClass::basicEventCallback, 10, 12.5, 100);
+    auto callbackPointer = mv::events::CallbackFactory{}.createBasicEventCallback(testObject,
+                                                                                  &TestClass::basicEventCallback, 10,
+                                                                                  12.5, 100);
     mv::events::CallbackFactory{}.removeCallback(callbackPointer);
     mv::events::CallbackFactoryTester tester;
-    ASSERT_EQ(tester.getNumBasicEventCallbacks(), 0);
+    ASSERT_EQ(tester.getNumCallbacks(), 0);
 }
 
+TEST(CallbackFactory, DataEventCallbackRemoval) {
+    TestClass testObject;
+    auto callbackPointer = mv::events::CallbackFactory{}.createDataEventCallback(testObject, &TestClass::dataEventCallback);
+    mv::events::CallbackFactory{}.removeCallback(callbackPointer);
+    mv::events::CallbackFactoryTester tester;
+    ASSERT_EQ(tester.getNumCallbacks(), 0);
+}
