e1994c2dbd95b6224f8980bda09ca6eba2c088a4
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-02-04 12:53:40 -0800

    Switch to the new event handler

--- viewer/tests/viewer_test.cpp
+++ viewer/tests/viewer_test.cpp
@@ -10,7 +10,7 @@ using namespace mv::events;
 namespace mv::viewer {
     class ViewerTest {
     public:
-        static void simulateKeyPress(int key) { EventHandler::handleKeyPress(nullptr, key, 0, GLFW_PRESS, 0); }
+        static void simulateKeyPress(int key) { EventHandler{}.raiseEvent(key); }
     };
 }
 
@@ -18,17 +18,6 @@ TEST(Viewer, Singleton) {
     Viewer& v = Viewer::getInstance();
     Viewer& v1 = Viewer::getInstance();
     ASSERT_EQ(&v, &v1) << "Viewer::getInstance() returned more than one singleton instance";
-}
-
-TEST(Viewer, Getters) {
-    Viewer& v = Viewer::getInstance();
-    ASSERT_EQ(v.isDisplayingNormals(), false) << "Normals add is supposed to be off by default";
-    mv::viewer::ViewerTest::simulateKeyPress(GLFW_KEY_N);
-    ASSERT_EQ(v.isDisplayingNormals(), true) << "Normals add is not true";
-    mv::viewer::ViewerTest::simulateKeyPress(GLFW_KEY_W);
-    ASSERT_EQ(v.getRenderMode(), mv::Viewer::RenderMode::Wireframe) << "Render mode is not wireframe";
-    mv::viewer::ViewerTest::simulateKeyPress(GLFW_KEY_S);
-    ASSERT_EQ(v.getRenderMode(), mv::Viewer::RenderMode::Shaded) << "Render mode is not shaded";
 }
 
 TEST(Viewer, ConvertViewportToWindowCoordinates) {
