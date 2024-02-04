ef01c9b7925e6cd2d82850700474f93bf47dc8b9
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-02-03 17:17:13 -0800

    Clean up: Unify data member names and const usage. Add a stream insertion overload to print event data to a stream

--- events/Event.h
+++ events/Event.h
@@ -7,38 +7,44 @@ namespace mv { namespace events {
 
 class Event { 
     public:
-        Event(const unsigned id, const unsigned modifier = 0) : m_id(id), m_modifier(modifier) {}
+        Event(unsigned const id, unsigned const modifier = 0) : id(id), modifier(modifier) {}
         ~Event() = default;
-        unsigned getId() const { return m_id; }
-        unsigned getModifier() const { return m_modifier; }
+        unsigned getId() const { return id; }
+        unsigned getModifier() const { return modifier; }
         
         // Copy construction is allowed. This allows an instance of Event to be copy constructed
         // and placed in the memory of lookup data structures that map an event to its callback
-        Event(Event const& other) : m_id(other.m_id), m_modifier(other.m_modifier) {}
+        Event(Event const& other) : id(other.id), modifier(other.modifier) {}
         // Move is strictly not necessary, but doesn't feel correct to support copy construction
         // and leave out move construction
-        Event(Event&& other) : m_id(other.m_id), m_modifier(other.m_modifier) {}
+        Event(Event&& other) : id(other.id), modifier(other.modifier) {}
         // No use case to assign one event to another
         Event& operator=(Event const&) = delete;
         Event& operator=(Event&& other) = delete;
 
         struct EventComparator {
             bool operator()(Event const& eventA, Event const& eventB) const {
-                return eventA.m_id == eventB.m_id &&
-                    eventA.m_modifier == eventB.m_modifier;
+                return eventA.id == eventB.id &&
+                       eventA.modifier == eventB.modifier;
             }
         };
 
         struct EventHasher {
             size_t operator()(Event const& event) const  {
-                return event.m_id;
+                return event.id;
             }
         };
 
     protected:
-        const unsigned m_id;
-        const int m_modifier;
+        const unsigned id;
+        const int modifier;
+
 };
+
+inline std::ostream& operator <<(std::ostream& os, Event const& event) {
+    os << "Event id = " << event.getId() << " modifiers = " << event.getModifier() << std::endl;
+    return os;
+}
 
 } }
 
