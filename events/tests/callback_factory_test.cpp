cc5af3fbc995bc3e8cf1c19fdf1a31a7ec222311
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-02-03 17:36:05 -0800

    Rename callback factory test after retiring the old implementation.

--- events/tests/callback_factory_test.cpp
+++ events/tests/callback_factory_test.cpp
@@ -1,65 +1,53 @@
 #include "gtest/gtest.h"
-#include "CallbackFactory_Old.h"
-#include <memory>
-using namespace mv::events;
-using namespace std;
+#include "Callback.h"
+#include "CallbackFactory.h"
 
-// Friend class implementation. This class is used to test the internal state of
-// the CallbackFactory object as callback functions are registered
-namespace mv { namespace events {
-class CallbackFactoryTester {
+namespace mv::events {
+    class CallbackFactoryTester {
     public:
-        CallbackFactoryTester(CallbackFactory_Old& cbf) : m_cbf(cbf) { }
-        size_t getNumberOfCallbacks() const { return m_cbf.m_callbackFunctions.size(); } 
-        void callFunction() { m_cbf.m_callbackFunctions.front()->call(); }
-    private:
-        CallbackFactory_Old& m_cbf;
-};
-} }
-
-void freeFunction(int a, float b, char c) {
-    cout << "freeFunction called with parameters " << a << ' ' << b << ' ' << c; 
-}
-
-class TestClass {
-    public:
-        void memberFunction(int a, float b, char c) {
-            cout << "TestClass::memberFunction called with parameters " << a << ' ' << b << ' ' << c;
+        auto getNumCallbacks() {
+            return mv::events::CallbackFactory{}.callbacks.size();
         }
-        ~TestClass() { cout << "Destructor" << endl; }
+    };
+}
+
+struct TestClass : public mv::MeshViewerObject {
+    void basicEventCallback(int a, float b, unsigned c) {
+    }
+
+    void dataEventCallback(std::vector<std::any>&& eventData) {
+    }
+
 };
 
-
-TEST(CallbackFactory_Old, Singleton) {
-    CallbackFactory_Old& cb1 = CallbackFactory_Old::getInstance();
-    CallbackFactory_Old& cb2 = CallbackFactory_Old::getInstance();
-    ASSERT_EQ(&cb1, &cb2) << "CallbackFactory::getInstance() is not returning a singleton" << std::endl;
+TEST(CallbackFactory, BasicEventCallbackCreation) {
+    TestClass testObject;
+    mv::events::CallbackFactory{}.createBasicEventCallback(testObject, &TestClass::basicEventCallback, 10, 12.5, 100);
+    mv::events::CallbackFactoryTester tester;
+    ASSERT_EQ(tester.getNumCallbacks(), 1);
 }
 
-TEST(CallbackFactory_Old, RegisterFreeFunction) {
-    CallbackFactory_Old::getInstance().registerCallback(&freeFunction, 5, 2.3f, 'x');
-    ASSERT_TRUE(CallbackFactoryTester(CallbackFactory_Old::getInstance()).getNumberOfCallbacks() == 1);
-    testing::internal::CaptureStdout();
-    CallbackFactoryTester(CallbackFactory_Old::getInstance()).callFunction();
-    ASSERT_EQ(testing::internal::GetCapturedStdout(), 
-              "freeFunction called with parameters 5 2.3 x") << "Free function not called as expected" << endl;
+TEST(CallbackFactory, DataEventCallbackCreation) {
+    TestClass testObject;
+    mv::events::CallbackFactory{}.createDataEventCallback(testObject, &TestClass::dataEventCallback);
+    mv::events::CallbackFactoryTester tester;
+    ASSERT_EQ(tester.getNumCallbacks(), 1);
 }
 
-TEST(CallbackFactory_Old, RegisterMemberFunction) {
-    TestClass t;
-    CallbackFactory_Old::getInstance().registerCallback(t, &TestClass::memberFunction, 15, 12.3f, 'y');
-    testing::internal::CaptureStdout();
-    CallbackFactoryTester(CallbackFactory_Old::getInstance()).callFunction();
-    ASSERT_EQ(testing::internal::GetCapturedStdout(), 
-              "TestClass::memberFunction called with parameters 15 12.3 y") << "Member function not called as expected" << endl;
+TEST(CallbackFactory, BasicEventCallbackRemoval) {
+    TestClass testObject;
+    auto callbackPointer = mv::events::CallbackFactory{}.createBasicEventCallback(testObject,
+                                                                                  &TestClass::basicEventCallback, 10,
+                                                                                  12.5, 100);
+    mv::events::CallbackFactory{}.removeCallback(callbackPointer);
+    mv::events::CallbackFactoryTester tester;
+    ASSERT_EQ(tester.getNumCallbacks(), 0);
 }
 
-TEST(CallbackFactory_Old, InstanceLifeCycleManagement) {
-    
-    TestClass t; 
-    CallbackFactory_Old::getInstance().registerCallback(t, &TestClass::memberFunction, 25, 22.3f, 'z');
-    testing::internal::CaptureStdout();
-    CallbackFactoryTester(CallbackFactory_Old::getInstance()).callFunction();
-    ASSERT_EQ(testing::internal::GetCapturedStdout(), 
-              "TestClass::memberFunction called with parameters 25 22.3 z") << "Member function not called as expected" << endl;
+TEST(CallbackFactory, DataEventCallbackRemoval) {
+    TestClass testObject;
+    auto callbackPointer = mv::events::CallbackFactory{}.createDataEventCallback(testObject, &TestClass::dataEventCallback);
+    mv::events::CallbackFactory{}.removeCallback(callbackPointer);
+    mv::events::CallbackFactoryTester tester;
+    ASSERT_EQ(tester.getNumCallbacks(), 0);
 }
