7820497ab634fcf1c133928c430a1c3b63037a5a
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-02-07 23:21:37 -0800

    Move all gl headers and conditional compilation logic into OpenGLCall.h

--- common/OpenGLCall.h
+++ common/OpenGLCall.h
@@ -1,6 +1,13 @@
 #pragma once
 #ifndef EMSCRIPTEN
 #include "GL/glew.h"
+#else
+#include "GLES2/gl2.h"
+#define GL_GLEXT_PROTOTYPES
+#include "GLES2/gl2ext.h"
+#define glBindVertexArray       glBindVertexArrayOES
+#define glGenVertexArrays       glGenVertexArraysOES
+#define glDeleteVertexArrays    glDeleteVertexArraysOES
 #endif
 #include <string>
 #include "Environment.h"
