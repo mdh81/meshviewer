c557de21dd0e1b780b44c8340a8704d73f551b2e
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-04-27 22:43:32 -0700

    Allow emscripten build in development environment to be built in either debug or release mode. To facilitate testing, allow the use of any browser that emrun has support for

--- embuild.sh
+++ embuild.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+buildType="Debug"
+browser="chromium"
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -t|--buildType) buildType="$2"; shift 2;;
+        -b|--browser) browser="$2"; shift 2;;
+        --) shift; break;;
+        -*) echo "Unknown option: $1" >&2; exit 1;;
+        *) break;;
+    esac
+done
 pushd ./build_web
-emcmake cmake -S ../ B . -DEMSCRIPTEN=On -DCMAKE_BUILD_TYPE=Debug &&
-make -j 8 VERBOSE=1
+emcmake cmake -S ../ B . -DEMSCRIPTEN=On -DCMAKE_BUILD_TYPE="$buildType" &&
+make -j 10 VERBOSE=1
 if [ $? -eq 0 ]; then
-  emrun --port 5600 --browser chromium meshViewer.html /testfiles/cylinder.stl &
+  emrun --port 5600 --browser "$browser" meshViewer.html /testfiles/suzanne_subdivided.stl &
 fi
 popd
 
