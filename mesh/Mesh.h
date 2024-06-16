2db60250272e7187c2ae204b845e621d3b32b4f9
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-06-15 23:27:27 -0700

    Mesh is now an interface and MeshImpl is the implementation. This allows a mock mesh to be injected into code that depends on mesh by their unit tests

--- mesh/Mesh.h
+++ mesh/Mesh.h
@@ -6,14 +6,13 @@
 #include <optional>
 #include <filesystem>
 #include "Types.h"
-#include "Drawable3D.h"
-#include "Octree.h"
 #include "Vertex.h"
 #include "Face.h"
+#include "Drawable3D.h"
 
 namespace mv {
 
-class Mesh : public Drawable3D {
+class Mesh : public virtual Drawable3D {
     public:
         using Vertices = std::vector<Vertex>;
         using Faces = std::vector<Face>;
@@ -23,112 +22,56 @@ class Mesh : public Drawable3D {
         using Meshes = std::vector<MeshPointer>;
 
     public:
-        Mesh();
-
-        ~Mesh() override = default;
-
-        Mesh(const Mesh &);
-
-        // TODO: Define copy assignment
-        //       Define move assignment
-        //       Define move constructor
-        //
-
-    public:
-        void render() override;
-
-        [[nodiscard]]
-        bool supportsGlyphs() const override { return true; }
-
-    public:
         // TODO: Allow creation of meshes without this call. Make addVertex
         // and addFace allocate memory as needed. Come up with resonable
         // default initial allocation
-        void initialize(unsigned numVertices, unsigned numFaces);
+        virtual void initialize(unsigned numVertices, unsigned numFaces) = 0;
 
         // Adds a new vertex to this mesh
-        void addVertex(float x, float y, float z);
+        virtual void addVertex(float x, float y, float z) = 0;
 
-        // Adds a new face that is described the specified vertex identifiers
+        // Adds a new face that is described with the specified vertex identifiers
         // NOTE: vertexIds are 0-based
-        void addFace(const std::initializer_list<unsigned>& vertexIds);
+        virtual void addFace(const std::initializer_list<unsigned>& vertexIds) = 0;
 
         // Merges coincident vertices and adjusts the connectivity data
         // accordingly. Return number of duplicates that were removed
-        unsigned removeDuplicateVertices();
+        virtual unsigned removeDuplicateVertices() = 0;
 
         // Gets the number of vertices in this mesh
-        [[nodiscard]] unsigned getNumberOfVertices() const { return m_numVertices; }
+        virtual unsigned getNumberOfVertices() const  = 0;
 
         // Gets the number of faces in this mesh
-        [[nodiscard]] unsigned getNumberOfFaces() const { return m_numFaces; }
+        virtual unsigned getNumberOfFaces() const = 0;
 
         // Gets all vertices in this mesh
-        [[nodiscard]] const Vertices& getVertices() const { return m_vertices; }
+        virtual const Vertices& getVertices() const = 0;
 
         // Gets all faces in this mesh
-        [[nodiscard]] const Faces& getConnectivity() const { return m_faces; }
+        virtual const Faces& getConnectivity() const = 0;
 
         // Returns a vertex at the specified index
-        [[nodiscard]] const Vertex& getVertex(unsigned vertexIndex) const;
+        virtual const Vertex& getVertex(unsigned vertexIndex) const = 0;
 
         // Returns a face at the specified index
-        [[nodiscard]] const Face& getFace(unsigned faceIndex) const;
-
-        // Gets the bounds of the mesh
-        [[nodiscard]] common::Bounds getBounds() const override;
-
-        // Gets the centroid of the mesh
-        [[nodiscard]] common::Point3D getCentroid() const override;
+        virtual const Face& getFace(unsigned faceIndex) const = 0;
 
         // Gets vertices
-        [[nodiscard]] VertexData getVertexData() const;
+        virtual VertexData getVertexData() const = 0;
 
         // Gets connectivity data in the form a pointer to a contiguous memory
-        void getConnectivityData(size_t& numBytes, unsigned*& connData) const;
+        virtual void getConnectivityData(size_t& numBytes, unsigned*& connData) const = 0;
 
         // Transforms a copy of this mesh and returns the copy
-        [[nodiscard]] std::unique_ptr<Mesh> transform(common::TransformMatrix const& transformMatrix) const;
+        virtual std::unique_ptr<Mesh> transform(common::TransformMatrix const& transformMatrix) const = 0;
 
         // Writes this mesh to a STL file
-        void writeToSTL(const std::string& stlFile) const;
+        virtual void writeToSTL(const std::string& stlFile) const = 0;
 
         // Gets normals
-        [[nodiscard]] NormalData getNormals(common::NormalLocation) const;
+        virtual NormalData getNormals(common::NormalLocation) const = 0;
 
-        void writeToFile(std::string const& fileName, common::TransformMatrix const& transform) const override {
-            std::filesystem::path filePath = fileName;
-            if (filePath.extension() == ".STL" || filePath.extension() == ".stl") {
-                this->transform(transform)->writeToSTL(fileName);
-            } else {
-                throw std::runtime_error("Currently, only STL output for meshes is supported.");
-            }
-        }
-
-protected:
-        void generateRenderData() override;
-        void generateColors() override;
-
-    private:
-        unsigned m_numVertices;
-        unsigned m_numFaces;
-        Vertices m_vertices;
-        Faces m_faces;
-        std::optional<common::Bounds> m_bounds;
-        std::optional<Octree> m_octree;
-        std::unique_ptr<unsigned> m_connectivity;
-        size_t m_connectivityDataSize;
-        std::optional<NormalData> m_vertexNormals;
-        std::optional<NormalData> m_faceNormals;
-        std::optional<VertexData> m_vertexData;
-
-    private:
-        void buildConnectivityData();
-        void buildBounds();
-        void generateVertexNormals();
-        void generateFaceNormals();
-        void buildVertexData();
-
+        virtual ~Mesh() = default;
 };
 
 }
