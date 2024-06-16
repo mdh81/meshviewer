902a6ed85af14d5651f5958e41ffbdb0f00b0e78
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-06-16 09:49:37 -0700

    Get concrete viewer from the viewer factory

--- viewer/tests/viewer_test.cpp
+++ viewer/tests/viewer_test.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
-#include "Viewer.h"
+#include "ViewerFactory.h"
 #include "EventHandler.h"
 #include <memory>
 using namespace std;
@@ -7,21 +7,8 @@ using namespace mv;
 using namespace mv::common;
 using namespace mv::events;
 
-namespace mv::viewer {
-    class ViewerTest {
-    public:
-        static void simulateKeyPress(int key) { EventHandler{}.raiseEvent(key); }
-    };
-}
-
-TEST(Viewer, Singleton) {
-    Viewer& v = Viewer::getInstance();
-    Viewer& v1 = Viewer::getInstance();
-    ASSERT_EQ(&v, &v1) << "Viewer::getInstance() returned more than one singleton instance";
-}
-
 TEST(Viewer, ConvertViewportToWindowCoordinates) {
-    Viewer& v = Viewer::getInstance(/*width=1024, height=768*/);
+    auto& v = viewer::ViewerFactory{}.getViewer();
     auto viewportToWindow = v.getViewportToWindowTransform();
     auto windowCoordinates = viewportToWindow * common::Point3D{1, 1, 1};
     ASSERT_FLOAT_EQ(windowCoordinates.x, 1024.f) << "Wrong window coordinates for viewport coordinate 1,1";
