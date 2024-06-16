972b51537c64f8e9d714f73d9d31556393a9fe00
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-06-15 23:55:26 -0700

    Use a mesh factory to build a mesh. This allows a mock mesh factory to be injected by reader tests to build a mock mesh and test the reader internals without building an elaborate real mesh and in the process make these tests dependent on all of mesh's dependencies.

--- readers/Reader.h
+++ readers/Reader.h
@@ -1,22 +1,23 @@
 #pragma once
 #include <memory>
+#include "MeshViewerObject.h"
+#include "MeshFactory.h"
 #include <utility>
 
-namespace mv {
-    class Mesh;
-}
-
 namespace mv::readers {
-using MeshPointer = std::unique_ptr<mv::Mesh>;
 
-class Reader {
+class Reader : public MeshViewerObject {
 public:
-    explicit Reader(std::string fileName) : fileName(std::move(fileName)) {}
-    virtual MeshPointer getOutput() = 0;
+    using MeshPointer = Mesh::MeshPointer;
+    explicit Reader(std::string fileName, IMeshFactory const& meshFactory)
+    : fileName(std::move(fileName))
+    , meshFactory(meshFactory) {}
+    virtual Mesh::MeshPointer getOutput(Mesh::MeshPointer = nullptr) = 0;
     virtual ~Reader() = default;
 
 protected:
     std::string const fileName;
+    IMeshFactory const& meshFactory;
 };
 
 }
