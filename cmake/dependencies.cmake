ad4979d812b94e69eeb6dc070c65975fede3448d
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-12-17 09:51:40 -0800

    Moved cmake files to a separate directory. Made dependency management easier by creating a common and dedicated cmake file for them

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,35 @@
+include(FetchContent)
+
+# Gtest
+if (enableTesting)
+    enable_testing()
+    FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG v1.15.2
+    )
+    FetchContent_MakeAvailable(googletest)
+    include(${CMAKE_SOURCE_DIR}/common/testsupport/TestSupport.cmake)
+endif()
+
+# Imgui
+# NOTE: Since Imgui doesn't include a CMakeLists.txt, it won't be
+# built, which is the desired outcome. FetchContent_MakeAvailable
+# will introduce imgui_SOURCE_DIR variable, which can be used to
+# include the sources in the meshviewer's modules
+FetchContent_Declare(
+    imgui
+    GIT_REPOSITORY https://github.com/ocornut/imgui.git
+    GIT_TAG v1.91.5
+)
+FetchContent_MakeAvailable(imgui)
+
+# GL libraries
+# TODO: Manage GLEW, GLFW3 through fetch content
+if (NOT DEFINED EMSCRIPTEN)
+    find_package(GLEW REQUIRED)
+    find_package(GLFW3 REQUIRED)
+    find_package(OpenGL REQUIRED)
+    # TODO: Get rid of GLM
+    find_package(glm REQUIRED)
+endif()
---
+++ cmake/emscripten.cmake
@@ -0,0 +1,80 @@
+include(${CMAKE_SOURCE_DIR}/cmake/dependencies.cmake)
+
+# set experimental flag on, without this std::format doesn't compile
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fexperimental-library")
+
+# set preprocessor flag
+add_compile_options(-DEMSCRIPTEN)
+
+# compile and link options
+add_compile_options(
+    --no-heap-copy
+    -fexceptions
+    -pthread
+)
+
+# Preloaded assets
+set(preloadedFiles "")
+# Sample files
+file(GLOB sampleFiles ${CMAKE_CURRENT_SOURCE_DIR}/testfiles/*)
+foreach(sampleFile ${sampleFiles})
+    get_filename_component(destinationFile ${sampleFile} NAME)
+    set(preloadedFiles "${preloadedFiles} --preload-file ${sampleFile}@testfiles/${destinationFile}")
+endforeach(sampleFile)
+# Object Textures TODO: Make this read from assets
+file(GLOB textureFiles ${CMAKE_CURRENT_SOURCE_DIR}/objects/textures/*)
+foreach(textureFile ${textureFiles})
+    get_filename_component(destinationFile ${textureFile} NAME)
+    set(preloadedFiles "${preloadedFiles} --preload-file ${textureFile}@textures/${destinationFile}")
+endforeach(textureFile)
+# UI Textures
+file(GLOB textureFiles ${CMAKE_CURRENT_SOURCE_DIR}/ui/textures/icons/*)
+foreach(textureFile ${textureFiles})
+    get_filename_component(destinationFile ${textureFile} NAME)
+    set(preloadedFiles "${preloadedFiles} --preload-file ${textureFile}@assets/textures/${destinationFile}")
+endforeach(textureFile)
+# Fonts
+file(GLOB fonts ${CMAKE_CURRENT_SOURCE_DIR}/ui/fonts/* )
+foreach(font ${fonts})
+    get_filename_component(destinationFile ${font} NAME)
+    set(preloadedFiles "${preloadedFiles} --preload-file ${font}@assets/fonts/${destinationFile}")
+endforeach(font)
+
+# Link options
+# NOTE: It is vital to use SHELL: prefix to ensure that preloaded file paths that are composed above
+# are passed as-is without cmake attempting to help by inserting opening and closing quotes
+add_link_options(
+    "SHELL: --use_preload_cache ${preloadedFiles}"
+    --embed-file ${CMAKE_CURRENT_SOURCE_DIR}/config/defaults.cfg@config/defaults.cfg
+    --emrun
+    -fexceptions
+    -pthread
+    -sUSE_WEBGL2=1
+    -sMIN_WEBGL_VERSION=2
+    -sMAX_WEBGL_VERSION=2
+    -sUSE_GLFW=3
+    -sFULL_ES2
+    -sEXPORTED_RUNTIME_METHODS=allocate
+    -sPTHREAD_POOL_SIZE=2
+    "SHELL:-sWASM=1 -sUSE_WEBGL2=1 -sALLOW_MEMORY_GROWTH=1 -sALLOW_TABLE_GROWTH=1 -sSTACK_SIZE=5MB"
+)
+
+# TODO: Remove this when glm is replaced with 3dmath
+list(APPEND glIncludeDirectories /usr/local/include)
+
+# Disable building tests, they are not relevant for web target
+set(enableTesting OFF)
+# Build html output
+set(CMAKE_EXECUTABLE_SUFFIX ".html")
+
+# Enable debug if requested
+if (DEBUG)
+    add_compile_options(-g3)
+    add_link_options(
+        -sASSERTIONS=1
+        -g3
+    )
+endif()
+
+# The web sub-directory contains emscripten specific sources
+#add_subdirectory(web)
