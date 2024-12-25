d9279590f243bebb7be3fce258b9d24a25b6f577
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-12-24 17:01:24 -0800

    Linux build: First cut of a test runner to overcome issues seen in gtest_discover_tests. This script finds all the test directories in the build directory and executes ctest on them. There are odd results that need to be investigated

---
+++ test.sh
@@ -0,0 +1,12 @@
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 <build dir>"
+    exit -1
+fi
+
+testDirs=$(find . -name CTestTestfile.cmake -exec dirname {} \; | grep -E -v "_deps|tests")
+for testDir in $testDirs; 
+do 
+    pushd "$testDir"
+    ctest .
+    popd 
+done
